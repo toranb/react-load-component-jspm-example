c4725f916b64938a3ac71bf5522305c044aaa7c9
toran billups <toranb@gmail.com>
2014-07-08 13:15:25 -0500

    updated readme/removed ajax-mixin/pro-mixin from inbox

--- app/inbox.js
+++ app/inbox.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactRouter from 'react-nested-router';
-import AjaxMixin from 'ajax-mixin';
 
 var Link = ReactRouter.Link;
 
 var Inbox = React.createClass({
     render: function() {
-        var mixin = new AjaxMixin();
-        mixin.xhr("/api/incoming", "GET").then(function(response) {
-            response.forEach(function(item) {
-                console.log(item);
-            });
-        });
         return (
             React.DOM.div(null, 
                 React.DOM.h1(null, "Inbox!"),
